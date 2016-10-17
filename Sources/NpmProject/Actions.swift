7d7b6e558f559e32cd1165b4321b248ec33f9606
Valentin Knabel <develop@vknabel.com>
2016-10-17 10:51:08 +0200

    Fixes template issues

--- Sources/NpmProject/Actions.swift
+++ Sources/NpmProject/Actions.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 public extension NpmAction {
     public static func package(main: InlineTemplate = "index.ts", keywords: [InlineTemplate] = [], scripts: [String: InlineTemplate] = ["test": "echo \"Error: no test specified\" && exit 1"], repositoryType: String = "git", devDepencencies: [InlineTemplate] = [], dependencies: [InlineTemplate] = []) -> NpmAction {
         return NpmAction(
-            Action.write(templateNamed: .package, to: "{{ package.json }}")
+            Action.write(templateNamed: .package, to: "package.json")
             .with(additional: { _, context in
                 return [
                     "main": try main.render(context)
@@ -12,15 +12,15 @@ public extension NpmAction {
         )
     }
 
-    public static func tslint() -> NpmAction {
-        return NpmAction(.write(templateNamed: .tslint, to: "{{ tslint.json }}"))
+    public static func tslint(to file: InlineTemplate = "tslint.json") -> NpmAction {
+        return NpmAction(.write(templateNamed: .tslint, to: file))
     }
 
-    public static func tsconfig() -> NpmAction {
-        return NpmAction(.write(templateNamed: .tsconfig, to: "{{ tsconfig.json }}"))
+    public static func tsconfig(to file: InlineTemplate = "tsconfig.json") -> NpmAction {
+        return NpmAction(.write(templateNamed: .tsconfig, to: file))
     }
 
-    public static func index(empty: Bool = true, to file: InlineTemplate = "{{ index.ts }}") -> NpmAction {
+    public static func index(empty: Bool = true, to file: InlineTemplate = "index.ts") -> NpmAction {
         return NpmAction(.write(templateNamed: .index, to: file))
     }
 
